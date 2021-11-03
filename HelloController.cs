88952d822d977ae620b37e71a4a3140dc97cea6c
NikitaTroyes <89306099+NikitaTroyes@users.noreply.github.com>
2021-11-03 10:21:13 +0100

    Add files via upload

---
+++ HelloController.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QueteRoutage.Controllers
+{
+   
+    public class HelloController : Controller
+    {
+        public String SayHello1()
+
+        {
+            return "Hello !";
+        }
+
+        public String YellHello1()
+        {
+            return "HELLO !";
+        }
+
+        [Route("/Say")]
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+
+        [Route("/Yell")]
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+    }
+}
